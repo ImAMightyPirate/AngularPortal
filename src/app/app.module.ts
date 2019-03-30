d487d1e5090ac96f09a0e0b3013fc2adf9b48e47
ImAMightyPirate <adammytton@gmail.com>
2019-03-30 22:53:07 +0000

    Work in progress on master-detail. Alternate colour scheme testing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { HeaderModule } from './modules/header/header.module';
 import { FooterModule } from './modules/footer/footer.module';
+import { MasterDetailModule } from './modules/master-detail/master-detail.module';
 import { NavigationModule } from './modules/navigation/navigation.module';
 
 import { AppComponent } from './app.component';
@@ -25,6 +26,7 @@ import { AppComponent } from './app.component';
 
     HeaderModule,
     FooterModule,
+    MasterDetailModule,
     NavigationModule
   ],
   declarations: 
--- src/app/modules/master-detail/master-detail.module.ts
+++ src/app/modules/master-detail/master-detail.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatButtonModule, MatPaginatorModule, MatTableModule, MatToolbarModule } from '@angular/material';
+
+import { MasterDetailComponent } from './master-detail/master-detail.component';
+
 @NgModule({
-  imports: [
-    CommonModule
+  imports:
+  [
+    CommonModule,
+    FontAwesomeModule,
+    MatButtonModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatToolbarModule
   ],
-  declarations: []
+  declarations: 
+  [
+    MasterDetailComponent
+  ],
+  exports:
+  [
+    MasterDetailComponent
+  ]
 })
 export class MasterDetailModule { }
